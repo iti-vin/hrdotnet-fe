64eaee698f15f2e2256d043dcd905f3c9e342c3e
intellismart-dev <hersvinlabastidaintellismart@gmail.com>
2025-04-03 13:33:13 +0800

    Assets and Models

---
+++ src/modules/menu/Overtime/assets/types.ts
@@ -0,0 +1,22 @@
+export interface PaginationI {
+  total: number;
+  pageSize: number;
+  recordsLength: number;
+  time: string;
+  currentPage: number;
+}
+
+export interface HeaderButtonProps {
+  normalBtn: { label: string; icon?: React.ReactNode; onClick: () => void };
+  popoverBtn: { label: string; icon: React.ReactNode; innerLabel?: string; fOnClick: () => void; sOnClick: () => void };
+}
+
+export interface Batch {
+  filings: BatchFilings[];
+}
+interface BatchFilings {
+  recordId: number;
+  employeeId: number;
+  companyId: number;
+  documentNo: string;
+}
