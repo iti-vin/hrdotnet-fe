66709988635538d53825f7fba7b6805f6bc32326
hersvinlabastida <hersvinlabastida@intellismartinc.com>
2025-04-03 13:40:47 +0800

    Pages for Overtime

---
+++ src/modules/menu/Overtime/pages/Request.tsx
@@ -0,0 +1,104 @@
+/**
+ * @version    HRDotNet(v.2.0.0)
+ * @author     Hersvin Fred De La Cruz Labastida
+ */
+
+import Container from "@/layout/main/container";
+
+import { Header, Filter, Table, Pagination, Modals } from "../components";
+
+import { useQuery } from "@tanstack/react-query";
+import { OvertimeResponse } from "../models/response";
+import { OvertimeServices } from "../services/api";
+import { useEffect } from "react";
+import { useOvertimeStore } from "../store";
+import { statusColors } from "@shared/assets/types/Global";
+import { IconFileText } from "@tabler/icons-react";
+import { DateTimeUtils } from "@shared/utils/DateTimeUtils";
+
+export default function Filings() {
+  const panel = "REQUEST";
+  const { loading, setLoading, time, setTime, storedFilters, storedPage } = useOvertimeStore();
+
+  const { data, isLoading, refetch } = useQuery<OvertimeResponse>({
+    queryKey: ["request_overtime", { ...storedFilters }, { ...storedPage }],
+    queryFn: async () => {
+      const startTime = performance.now();
+      const result = await OvertimeServices.getAllMyOT({ ...storedFilters, ...storedPage });
+      const endTime = performance.now();
+      const executionTime = (endTime - startTime) / 1000;
+      setTime(executionTime.toFixed(3).toString());
+      return result;
+    },
+    staleTime: 1000 * 60 * 5,
+  });
+
+  useEffect(() => {
+    setLoading(isLoading);
+    refetch();
+  }, [loading]);
+
+  return (
+    <Container>
+      <Header panel={panel} />
+      <Filter panel={panel} />
+
+      <Table
+        records={data && data.items}
+        isLoading={isLoading}
+        columns={[
+          { accessor: "filing.documentNo", title: "Document No" },
+          { accessor: "filing.shiftSchedule.name", title: "Schedule" },
+          { accessor: "filing.shiftSchedule.date", title: "Overtime Date" },
+          { accessor: "filing.shiftSchedule.timeIn", title: "Overtime Hours" },
+          {
+            accessor: "filing.dateTransaction",
+            title: "Transaction Date",
+            render: (row: any) => DateTimeUtils.getIsoDateWord(row.filing.dateTransaction),
+          },
+          { accessor: "name", title: "Processed By" },
+          {
+            accessor: "filing.filingStatus.name",
+            title: "Status",
+            textAlign: "center",
+            width: "150px",
+            render: (row: any) => {
+              const statusInfo = statusColors.find((item) => item.status === row.filing.filingStatus.name) || {
+                status: "Unknown",
+                color: "gray",
+              };
+              return (
+                <div className="rounded-xl text-center p-1" style={{ background: statusInfo.color, color: "white" }}>
+                  {row.filing.filingStatus.name}
+                </div>
+              );
+            },
+          },
+          {
+            accessor: "attachment",
+            title: "Attachment",
+            textAlign: "center",
+            render: () => (
+              <div className="flex justify-center hover:scale-105">
+                <IconFileText onClick={(e) => e.stopPropagation()} color="rgba(109, 109, 109, 0.6)" />
+              </div>
+            ),
+          },
+        ]}
+        panel={panel}
+      />
+
+      {data && (
+        <Pagination
+          total={Math.ceil(data.total / data.pageSize)}
+          pageSize={data.pageSize}
+          recordsLength={data.total}
+          currentPage={data.page}
+          time={time}
+        />
+      )}
+
+      <Modals panel={panel} />
+    </Container>
+  );
+}
