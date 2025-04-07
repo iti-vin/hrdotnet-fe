f4ce9e4f8b27a5bcf40c35ade6eb0f483781cfdc
hersvinlabastida <hersvinlabastida@intellismartinc.com>
2025-04-07 09:58:14 +0800

    Fixed Container

--- src/modules/menu/ChangeSchedule/index.tsx
+++ src/modules/menu/ChangeSchedule/index.tsx
@@ -20,15 +20,21 @@ function COSContent() {
   return (
     <React.Fragment>
       <title>Change of Schedule</title>
-      <PanelNav>
-        {cosTabs.map((item) => (
-          <NavLink key={item.index} to={item.path} className={({ isActive }) => (isActive ? "active" : "inactive")} onClick={() => setActiveTab(item.index)}>
-            {item.label}
-          </NavLink>
-        ))}
-      </PanelNav>
-      <Stack className="bg-white m-4 mt-16 -mb-16 h-screen-85 overflow-y-hidden -z-10 p-8 rounded-lg bottom-0 select-none">
-        <Outlet />
+      <Stack className="h-full">
+        <PanelNav>
+          {cosTabs.map((item) => (
+            <NavLink
+              key={item.index}
+              to={item.path}
+              className={({ isActive }) => (isActive ? "active" : "inactive")}
+              onClick={() => setActiveTab(item.index)}>
+              {item.label}
+            </NavLink>
+          ))}
+        </PanelNav>
+        <Stack className="h-full bg-white mx-4 mt-16 p-8 rounded-lg select-none">
+          <Outlet />
+        </Stack>
       </Stack>
     </React.Fragment>
   );
--- src/modules/menu/MissedLog/index.tsx
+++ src/modules/menu/MissedLog/index.tsx
@@ -27,15 +27,21 @@ function MissedLogContent() {
   return (
     <Fragment>
       <title>Missed Log</title>
-      <PanelNav>
-        {missedLogTabs.map((item) => (
-          <NavLink key={item.index} to={item.path} className={({ isActive }) => (isActive ? "active" : "inactive")} onClick={() => setActiveTab(item.index)}>
-            {item.label}
-          </NavLink>
-        ))}
-      </PanelNav>
-      <Stack className="bg-white m-4 mt-16 -mb-16 h-screen-85 overflow-y-hidden -z-10 p-8 rounded-lg bottom-0 select-none">
-        <Outlet />
+      <Stack className="h-full">
+        <PanelNav>
+          {missedLogTabs.map((item) => (
+            <NavLink
+              key={item.index}
+              to={item.path}
+              className={({ isActive }) => (isActive ? "active" : "inactive")}
+              onClick={() => setActiveTab(item.index)}>
+              {item.label}
+            </NavLink>
+          ))}
+        </PanelNav>
+        <Stack className="h-full bg-white mx-4 mt-16 p-8 rounded-lg select-none">
+          <Outlet />
+        </Stack>
       </Stack>
     </Fragment>
   );
