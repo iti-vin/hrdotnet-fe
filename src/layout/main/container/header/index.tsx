10afdf179b93b517fe92f061dd45baef650e92be
hersvinlabastida <hersvinlabastida@intellismartinc.com>
2025-07-09 11:48:21 +0800

    header

--- src/layout/main/container/header/index.tsx
+++ src/layout/main/container/header/index.tsx
@@ -1,4 +1,6 @@
-import { Button, Flex, Popover, Stack, Text } from "@mantine/core";
+import { Flex, Popover, Stack, Text } from "@mantine/core";
+import { Button } from "@shared/components/button";
+import useResponsive from "@shared/hooks/useResponsive";
 import { IconCircleCheck, IconFilePlus, IconForbid2 } from "@tabler/icons-react";
 import React from "react";
 
@@ -25,16 +27,14 @@ interface HeaderProps {
 export default function Header({ ...props }: HeaderProps) {
   const [opened, setOpened] = React.useState(false);
 
+  const { isSmallHeight, isSmallWidth } = useResponsive();
+
   const handleClose = () => setOpened(false);
 
   const { title = "Title Header", normalBtn, popoverBtn } = props;
   return (
-    <Flex
-      direction={{ base: "column", sm: "row" }}
-      justify={{ base: "center", sm: "space-between" }}
-      align={{ base: "center", sm: "space-between" }}
-      className="w-full">
-      <Text className="text-xl font-semibold text-[#559CDA]" fz={22}>
+    <Flex direction={{ base: "column", sm: "row" }} justify={{ base: "center", sm: "space-between" }} align={{ base: "center", sm: "space-between" }} className="w-full">
+      <Text className="text-xl font-semibold text-[#559CDA]" fz={isSmallHeight || isSmallWidth ? 20 : 22}>
         {title}
       </Text>
       <Flex justify="center" gap={13}>
@@ -42,7 +42,8 @@ export default function Header({ ...props }: HeaderProps) {
           <Button
             className="flex justify-between"
             radius="md"
-            onClick={normalBtn.onClick}
+            type="button"
+            onClick={normalBtn.onClick!}
             color="#559CDA"
             leftSection={normalBtn.icon ? normalBtn.icon : <IconFilePlus size={25} stroke={2} />}
             h={36}>
@@ -52,13 +53,7 @@ export default function Header({ ...props }: HeaderProps) {
         {popoverBtn && (
           <Popover width={200} trapFocus position="bottom" withArrow shadow="md" opened={opened} onChange={setOpened}>
             <Popover.Target>
-              <Button
-                className="flex justify-between"
-                radius="md"
-                color="#559CDA"
-                leftSection={popoverBtn.icon}
-                h={36}
-                onClick={() => setOpened((o) => !o)}>
+              <Button className="flex justify-between" radius="md" color="#559CDA" leftSection={popoverBtn.icon} h={36} onClick={() => setOpened((o) => !o)}>
                 {popoverBtn.label}
               </Button>
             </Popover.Target>
